2efbd62120738037d276ef5f5045cc36f7803a50
adesilvadev <adesilva.dev@gmail.com>
2022-11-24 19:58:51 -0300

    JS - Importação e Exportação

---
+++ aula6_funcoes-auxiliares.js
@@ -0,0 +1,19 @@
+//-------------------------------------------------------------------------------------------------
+// IMPORTAÇÃO
+//-------------------------------------------------------------------------------------------------
+
+// EXEMPLO 1: BASE SIMPLES
+
+/*
+function gets() {
+
+    return 10;
+}
+
+function print(texto) {
+
+    console.log(texto);
+}
+
+module.exports = {gets, print}; // EXPORTA AS FUNÇÕES PARA QUE 'MAIN' POSSA IMPORTAR. 
+*/
---
+++ aula6_main.js
@@ -0,0 +1,6 @@
+
+/*
+const funcoes = require('./aula6_funcoes-auxiliares'); // IMPORTANDO AS FUNÇÕES PASSANDO 'require'.
+
+console.log(funcoes.print()); 
+*/
